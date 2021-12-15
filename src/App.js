fc34e0afdba39d0fbcfa22093b13aa8be053f4b1
SeilorMoon <nastya16.com@gmail.com>
2021-12-15 21:32:13 +0300

    change1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
 
 
 const App = () => {
