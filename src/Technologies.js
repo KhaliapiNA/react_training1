0ece5dca00a800acd2fcb5681b539684e09eac94
SeilorMoon <nastya16.com@gmail.com>
2021-12-15 18:11:31 +0300

    delete space

---
+++ src/Technologies.js
@@ -0,0 +1,12 @@
+const Technologies = function () {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+            </ul>
+        </div>
+    );
+}
+    export default Technologies;
