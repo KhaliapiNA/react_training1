0c862a6b004287f684337c923a69dd142c98f114
SeilorMoon <nastya16.com@gmail.com>
2022-02-08 11:27:45 +0200

    take out the data of array to index.js

---
+++ src/data.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+let data= {
+    profilePage: {
+    posts: [
+        {id: 1, message: 'I am so ate', likesCount: 33},
+        {id: 2, message: 'Leave place for dessert', likesCount: 23},
+        {id: 3, message: 'It\'s not fair', likesCount: 13}
+    ]},
+    messagesPage:{
+    dialogsData: [
+        {id: 'minako', name: 'Minako'},
+        {id: 'rei', name: 'Rei'},
+        {id: 'ami', name: 'Ami'},
+        {id: 'makoto', name: 'Makoto'},
+        {id: 'michiru', name: 'Michiru'}
+    ],
+    messagesData: [
+        {id: 1, message: 'A new department store has opened in the city'},
+        {id: 2, message: 'Can we go?'},
+        {id: 3, message: 'Sure'}
+    ]}
+};
+export default data;
--- src/index.js
+++ src/index.js
@@ -3,28 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import data from "./data";
 
-let posts = [
-    {id: 1, message: 'I am so ate', likesCount: 33},
-    {id: 2, message: 'Leave place for dessert', likesCount: 23},
-    {id: 3, message: 'It\'s not fair', likesCount: 13}
-];
-let dialogsData = [
-    {id: 'minako', name: 'Minako'},
-    {id: 'rei', name: 'Rei'},
-    {id: 'ami', name: 'Ami'},
-    {id: 'makoto', name: 'Makoto'},
-    {id: 'michiru', name: 'Michiru'}
-];
-
-let messagesData = [
-    {id: 1, message: 'A new department store has opened in the city'},
-    {id: 2, message: 'Can we go?'},
-    {id: 3, message: 'Sure'}
-];
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogsData} messages={messagesData}/>
+    <App posts={data.profilePage.posts} dialogs={data.messagesPage.dialogsData} messages={data.messagesPage.messagesData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
