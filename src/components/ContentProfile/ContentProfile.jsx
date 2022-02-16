baff97532452c15820e230fb0f09a22860643fd4
SeilorMoon <nastya16.com@gmail.com>
2022-02-16 17:57:38 +0200

    FLUX loop per character 2.0 worked

--- src/components/ContentProfile/ContentProfile.jsx
+++ src/components/ContentProfile/ContentProfile.jsx
@@ -13,10 +13,10 @@ const ContentProfile = function (props) {
             <img src='https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5b308264-dd6e-449d-b589-1d067d8184f5/d30efj7-1be94a19-acbb-493b-bafc-62c58378a190.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzViMzA4MjY0LWRkNmUtNDQ5ZC1iNTg5LTFkMDY3ZDgxODRmNVwvZDMwZWZqNy0xYmU5NGExOS1hY2JiLTQ5M2ItYmFmYy02MmM1ODM3OGExOTAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.I25b1K8Is7MahRI_5qEfyeXl6jy-fEQAKLx3ys34v8E' className={s.img1} />
 
             <Description /><br/>
-            <MyPosts posts={props.profilePage.posts}
-                     newPostText={props.profilePage.newPostText}
+            <MyPosts posts={props.posts}
+                     newPostText={props.newPostText}
                      updateNewPostText={props.updateNewPostText}
-                     addPost={props.state.addPost} />
+                     addPost={props.addPost} />
 
 
         </div>);
